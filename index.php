17df062151ab1a7732e59c855d3a3773eb18b6aa
nexseedcamille <ykamimori@nexseed.net>
2019-06-06 13:27:56 +0800

    問2の回答

--- index.php
+++ index.php
@@ -2,18 +2,22 @@
 $kid = new Kid();
 $kid->setName('しずか');
 $kid->setSex('girl');
+$kid->setAge(12);
 
 echo $kid->showName();
 
 class Kid{
   private $name;
   private $sex;
+  // 年齢を追加
+  private $age;
 
   // コンストラクタ
   function __construct(){
-    // 初期値の設定
     $this->name = 'Seed';
     $this->sex = 'boy';
+    // 初期値の設定
+    $this->age = 10;
   }
 
   public function getName(){
@@ -32,14 +36,39 @@ class Kid{
      $this->sex = $seibetsu;
    }
 
+  public function getAge(){
+    return $this->age;
+  }
+
+  public function setAge($nenrei){
+     $this->age = $nenrei;
+   }
+
   // 性別によって名前の語尾を判定するメソッド
   public function showName(){
+    // 名前の語尾を格納する変数
+    $suffix = '';
+
     if ($this->sex == 'boy') {
-      return $this->name . 'くん';
+      if ($this->age >= 12) {
+        // 男の子で12歳以上の場合
+        $suffix = '君';
+      } else {
+        // 男の子で11歳以下の場合
+        $suffix = 'くん';
+      }
     } else if($this->sex == 'girl') {
-      return $this->name . 'ちゃん';
+      if ($this->age >= 12) {
+        // 女の子で12歳以上の場合
+        $suffix = 'さん';
+      } else {
+        // 女の子で11歳以下の場合
+        $suffix = 'ちゃん';
+      }
     } else {
       return $this->name .'の性別が不明です';
     }
+    return $this->name . $suffix;
   }
+
 }
